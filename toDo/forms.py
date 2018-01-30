5a5ad48475da283a0a2453e4b844fe873501e677
kunduzyazilim@gmail.com <kunduzyazilim@gmail.com>
2018-01-30 17:02:00 +0200

    Vers:0.0003

--- toDo/forms.py
+++ toDo/forms.py
@@ -1,9 +1,12 @@
+#-*- coding: utf-8 -*-
+
 from django import forms
 
 class toDoListForm (forms.Form):
-    gorev = forms.CharField(widget=forms.TextInput(
-        attrs={"id":"myInput",
+    gorev = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={
+               "id":"myInput",
                "placeholder":"Görev Bilgisi...",
-               "name":"gorev"}
-    )
+               "name":"gorev"})
     )
--- toDo/views.py
+++ toDo/views.py
@@ -1,16 +1,17 @@
-#
+#-*- coding: utf-8 -*-
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import toDoListForm
 
 def toDo_List(request):
-    toDoList_Form = toDoListForm()
+    toDoList_Form = toDoListForm(request.POST or None)
     context = {
-        "PageTitle":"CyprusBooking To-Do-List",
+        "PageTitle": "CyprusBooking To-Do-List",
         "form": toDoList_Form,
     }
+    if toDoList_Form.is_valid():
+        print(toDoList_Form.cleaned_data)
     if request.method == "POST":
-        print(request.POST)
         print(request.POST.get('gorev'))
     return render(request,'index.html',context)
